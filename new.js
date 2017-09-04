bd8dc8be176c4d8c7d7e609af6f10a64c0aab07b
ammarkasem2016 <30480319+ammarkasem2016@users.noreply.github.com>
2017-09-04 23:48:25 +0200

    new.js
    new.js

---
+++ new.js
@@ -0,0 +1,33 @@
+let x = 1
+x = 7.25; {
+	console.log('x before rounding')
+}
+console.log(x)
+x = Math.round(x)
+console.log('x after rounding')
+let colors = ["red", "green", "blue"]
+let str = "Hello World!"
+let str_length = str.length
+let colors_length = colors.length
+console.log('The length of my array is : ',colors.length,'My array is : ',colors)
+let y = 'Hello'
+let z = 'ammar'
+if (typeof (y) === typeof (z)) {
+	console.log('Same Type');
+}
+else
+{
+	console.log('not the same type');
+}
+let m = 7
+m = m % 3
+console.log('The result of statement: (m = m % 3) if m = 7 is : ',m)
+
+let arr = [5, "green", "blue" , 8];
+
+if(6/0 == 10/0) {
+	console.log('Equal');
+}
+else {
+	console.log('Not Equal');
+}
